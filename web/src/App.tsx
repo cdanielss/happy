c1eab397bfa8b8efbb85226f61bef990e54abaef
cdanielss <cdanielss1973@outlook.com>
2020-10-12 12:12:15 -0300

    Primeira Pagina

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { FiArrowRight} from 'react-icons/fi';
 
+import './styles/global.css';
+import './styles/pages/landing.css'
+import logoImg from './images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo"/>
+        <main>
+          <h1>Leve felicidade para o Mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+        <div className="location">
+          <strong>Rio do Sul</strong>
+          <span>Santa Catarina</span>
+        </div>
+        
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
