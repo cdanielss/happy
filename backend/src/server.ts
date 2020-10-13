aa3d24b64b173f387298eeaaa2dd5828bf6c8d1a
cdanielss <cdanielss1973@outlook.com>
2020-10-13 09:55:20 -0300

    Criacao e Conexao com Banco de Dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,18 @@
 import express from 'express';
+import './database/connection';
 
 const app = express();
+app.use(express.json()); /* Para retornar Json */
 
 app.get('/', (req, res) => {
     return res.send("Hello");
 });
 
-app.listen(3333);
+app.listen(3000);
 
 
 
-/* Executar typescript para backend */
+/* Executar typescript em uma api backend */
 /* 
 1 - yarn init -y
 2 - yarn add express
@@ -18,4 +20,9 @@ app.listen(3333);
 4 - yarn add @types/express -D
 5 - yarn tsc --init
 6 - yarn add ts-node-dev -D
+*/
+
+/* Execucao do Banco de dados */
+/* 
+1 - yarn add typeorm sqlite3 
 */
