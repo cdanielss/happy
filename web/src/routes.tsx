ebeddb786ee7c34a2f0ee76737421bcb6b6f0362
cdanielss <cdanielss1973@outlook.com>
2020-10-14 18:00:44 -0300

    Adicionando Paginas e Tag

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route} from 'react-router-dom'; /* Faz o direcionamento para outras paginas */
 import Landing from './pages/Landing'; /* Uma pagina */
 import OrphanagesMap from './pages/OrphanagesMap'; /* Uma pagina */
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes() {
     return (
@@ -9,6 +11,8 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={OrphanagesMap} />
+                <Route path="/orphanages/create" component={CreateOrphanage} />
+                <Route path="/orphanages/:id" component={Orphanage} />
             </Switch>
         </BrowserRouter>
     );
