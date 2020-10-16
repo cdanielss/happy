cea51c48c0f394c7eb6dc981397ad53474adb8f8
cdanielss <cdanielss1973@outlook.com>
2020-10-16 11:54:29 -0300

    Salvando dados no banco e Finalizado Frontend

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import { FiPlus } from "react-icons/fi";
 import '../styles/pages/create-orphanage.css';
@@ -6,9 +6,36 @@ import Sidebar from "../components/Sidebar";
 import { LeafletMouseEvent } from 'leaflet';
 
 import mapIcon from '../utils/mapIcon'
+import api from "../services/api";
+import { useHistory } from "react-router-dom";
 
 export default function CreateOrphanage() {
-  const [position, setPosition] = useState({ latitude:0, longitude:0})
+  const history = useHistory();
+  const [position, setPosition] = useState({ latitude:0, longitude:0});
+  const [name, setName] = useState('');
+  const [about, setAbout] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [opening_hours, setOpeningHours] = useState('');
+  const [open_on_weekends, setOpenWeek] = useState(true);
+  const [images, setImages] = useState<File[]>([]);
+  const [previewImages, setPreview] = useState<string[]>([]);
+
+  function handleSelectImages(event: ChangeEvent<HTMLInputElement>){
+    if (!event.target.files) {
+      return;
+    }
+    
+    const selectdImages = Array.from(event.target.files);
+
+    setImages(selectdImages);
+
+    const selectdImagesPreview = selectdImages.map(image => {
+      return URL.createObjectURL(image);
+    })
+
+    setPreview(selectdImagesPreview);
+  }
+
   function handlerMapClick(event: LeafletMouseEvent) {
     const { lat, lng } = event.latlng;
 
@@ -17,12 +44,32 @@ export default function CreateOrphanage() {
       longitude: lng,
     });
   }
+
+  async function handleSubmit(event: FormEvent){
+    const {latitude, longitude} = position;
+    const data = new FormData(); 
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
+    images.forEach(image => {
+      data.append('images', image);
+    })
+
+    await api.post('/orphanages', data);
+    alert('Cadastro Realizado com Sucesso!');
+    history.push('/app');
+  }
+
   return (
     <div id="page-create-orphanage">
       <Sidebar/>
 
       <main>
-        <form className="create-orphanage-form">
+        <form onSubmit={handleSubmit} className="create-orphanage-form">
           <fieldset>
             <legend>Dados</legend>
 
@@ -45,24 +92,28 @@ export default function CreateOrphanage() {
 
             <div className="input-block">
               <label htmlFor="name">Nome</label>
-              <input id="name" />
+              <input id="name" value={name} onChange={event => setName(event.target.value)} />
             </div>
 
             <div className="input-block">
               <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
-              <textarea id="name" maxLength={300} />
+              <textarea id="name" maxLength={300} value={about} onChange={event => setAbout(event.target.value)}/>
             </div>
 
             <div className="input-block">
               <label htmlFor="images">Fotos</label>
 
-              <div className="uploaded-image">
-
+              <div className="images-container">
+                {previewImages.map(image => {
+                  return (
+                    <img key={image} src={image} alt={name}></img>
+                  )
+                })}
+                <label htmlFor="image[]" className="new-image">
+                  <FiPlus size={24} color="#15b6d6" />
+                </label>
               </div>
-
-              <button className="new-image">
-                <FiPlus size={24} color="#15b6d6" />
-              </button>
+              <input multiple onChange={handleSelectImages} type="file" id="image[]"/>
             </div>
           </fieldset>
 
@@ -71,20 +122,20 @@ export default function CreateOrphanage() {
 
             <div className="input-block">
               <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
+              <textarea id="instructions" value={instructions} onChange={event => setInstructions(event.target.value)} />
             </div>
 
             <div className="input-block">
-              <label htmlFor="opening_hours">Nome</label>
-              <input id="opening_hours" />
+              <label htmlFor="opening_hours">Horário de Funcionamento</label>
+              <input id="opening_hours" value={opening_hours} onChange={event => setOpeningHours(event.target.value)}/>
             </div>
 
             <div className="input-block">
               <label htmlFor="open_on_weekends">Atende fim de semana</label>
 
               <div className="button-select">
-                <button type="button" className="active">Sim</button>
-                <button type="button">Não</button>
+                <button type="button" className={open_on_weekends ? 'active' : ''} onClick={() => setOpenWeek(true)}>Sim</button>
+                <button type="button" className={!open_on_weekends ? 'active' : ''} onClick={() => setOpenWeek(false)}>Não</button>
               </div>
             </div>
           </fieldset>
